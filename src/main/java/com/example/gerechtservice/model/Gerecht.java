1bb54e12311ac533903c8cf7854f29f9b172b40b
Toon Staes <r0784094@student.thomasmore.be>
2021-12-28 14:38:11 +0100

    made sure name is unique

--- src/main/java/com/example/gerechtservice/model/Gerecht.java
+++ src/main/java/com/example/gerechtservice/model/Gerecht.java
@@ -1,12 +1,14 @@
 package com.example.gerechtservice.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "gerechten")
 public class Gerecht {
     @Id
     private String id;
+    @Indexed(unique=true)
     private String naam;
     private double prijs;
 
